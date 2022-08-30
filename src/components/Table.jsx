1d5c0706ccd8ecae9aa7d77d75004019f8bf72d2
Ludson <ludson_ps27@hotmail.com>
2022-08-30 11:26:36 -0300

    Requisito 7

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    console.log('Eu sou o expenses: ', expenses[0]);
     return (
-      <div>
-        <table>
-          <thead>
+      <table>
+        <thead>
+          <tr>
             <th>Descrição</th>
             <th>Tag</th>
             <th>Método de pagamento</th>
@@ -15,11 +19,39 @@ class Table extends Component {
             <th>Valor convertido</th>
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
-          </thead>
-        </table>
-      </div>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses && expenses.map((coin) => {
+            console.log(coin.exchangeRates[coin.currency]);
+            const { name, ask } = coin.exchangeRates[coin.currency];
+            const teste = name.split('/');
+            console.log('coin: ', Number(ask).toFixed(2));
+            return (
+              <tr key={ expenses.id }>
+                <td>{coin.description}</td>
+                <td>{coin.tag}</td>
+                <td>{coin.method}</td>
+                <td>{Number(coin.value).toFixed(2)}</td>
+                <td>{name}</td>
+                <td>{Number(ask).toFixed(2)}</td>
+                <td>{(Number(coin.value) * Number(ask)).toFixed(2)}</td>
+                <td>{teste[1]}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.string,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
